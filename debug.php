cba8fa7860cb59e7287d845c0f623308367a4c56
scanniza <simon@cymonit.se>
2015-03-15 01:41:33 +0100

    data

---
+++ debug.php
@@ -0,0 +1,6 @@
+$curl = curl_init('/hhh.html');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.10 (KHTML, like Gecko) Chrome/8.0.552.224 Safari/534.10');
+$html = curl_exec($curl);
+curl_close($curl);
